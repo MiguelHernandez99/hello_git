30f6aa94291f47dfa873ebc59667cb03a18ef3e3
user90 <loco056.eh@gmail.com>
2026-09-09 12:09:18 -0600

    hola git, agregue un fibonacci

--- testgit.py
+++ testgit.py
@@ -1,3 +1,20 @@
 print("hola git")
 print("un cambio")
 #otro cambio
+#---------------------->
+cantidad_numeros = int(input("¿Cuantos numeros de Fibonacci quieres?"))
+
+if cantidad_numeros <= 0:
+	print('El numero debe ser > a 0!!!')
+else:
+	a = 0
+	b = 1
+	print('Los primeros', cantidad_numeros, "numeros de Fibonacci son:")
+	for i in range(cantidad_numeros):
+		print(a)
+		siguiente = a + b
+		a = b
+		b = siguiente
+
+
+
